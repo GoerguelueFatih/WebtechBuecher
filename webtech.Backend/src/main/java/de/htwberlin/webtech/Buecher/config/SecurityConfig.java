aaa7f4c29ce3df9979916b7687c37c8fbbd2dab2
GoerguelueFatih <107949729+GoerguelueFatih@users.noreply.github.com>
2024-01-13 20:48:48 +0100

    Cleanup not needed code

--- webtech.Backend/src/main/java/de/htwberlin/webtech/Buecher/config/SecurityConfig.java
+++ webtech.Backend/src/main/java/de/htwberlin/webtech/Buecher/config/SecurityConfig.java
@@ -2,12 +2,15 @@ package de.htwberlin.webtech.Buecher.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -16,18 +19,17 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors().and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/**").authenticated()
-                        .anyRequest().permitAll()
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/books/search").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer().jwt();
-
-        return http.build();
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
@@ -36,4 +38,3 @@ public class SecurityConfig {
     }
 
 }
-
